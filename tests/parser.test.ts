7a7a7f1be2ec92a9fda475a0b3692deb38b28710
RickaPrincy <rckprincy@gmail.com>
2025-04-03 19:53:44 +0300

    test: parse with operator

--- tests/parser.test.ts
+++ tests/parser.test.ts
@@ -46,4 +46,76 @@ describe('Parser', () => {
       ],
     });
   });
+
+  test('should parse declartion with operator ', () => {
+    const input = 'const myNumber: number = 50 + 30';
+    const ast = parser.parse(input);
+
+    expect(ast).toEqual({
+      type: 'ProgramNode',
+      body: [
+        {
+          type: 'VariableDeclarationNode',
+          identifier: "myNumber",
+          valueType: "number",
+          value: {
+            type: "BinaryExpressionNode",
+            operator: "+",
+            left: {
+              type: "NumericLiteral",
+              value: 50
+            },
+            right: {
+              type: "NumericLiteral",
+              value: 30
+            },
+          }
+        },
+      ],
+    });
+  });
+
+  test('should parse declartion with multiple operator ', () => {
+    const input = 'const myNumber: number = 50 + 30 - 39 + 15';
+    const ast = parser.parse(input);
+
+    expect(ast).toEqual({
+      "type": "ProgramNode",
+      "body": [
+        {
+          "type": "VariableDeclarationNode",
+          "value": {
+            "type": "BinaryExpressionNode",
+            "operator": "+",
+            "left": {
+              "type": "BinaryExpressionNode",
+              "operator": "-",
+              "left": {
+                "type": "BinaryExpressionNode",
+                "operator": "+",
+                "left": {
+                  "type": "NumericLiteral",
+                  "value": 50
+                },
+                "right": {
+                  "type": "NumericLiteral",
+                  "value": 30
+                }
+              },
+              "right": {
+                "type": "NumericLiteral",
+                "value": 39
+              }
+            },
+            "right": {
+              "type": "NumericLiteral",
+              "value": 15
+            }
+          },
+          "identifier": "myNumber",
+          "valueType": "number"
+        }
+      ]
+    });
+  });
 });
